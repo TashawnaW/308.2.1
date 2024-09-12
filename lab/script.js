42ea306374b99c67630b0d3b48c454943527d051
TashawnaW <tashawnawells@gmail.com>
2024-09-11 23:57:27 -0400

    Added constants

---
+++ lab/script.js
@@ -0,0 +1,29 @@
+const PI=31415;
+const Radius=5;
+const Plant_Space=0.8;
+const Area = PI * radius * radius;
+const Initial_Plants=20
+const Number_Plants=(n)Initial_Plants*2
+let n=number of weeks
+
+console console.log(PlantGrowth);1*Initial_Plants*2
+
+
+
+
+
+The area is starting with 20 plants.
+
+
+
+The plants double in number every week.
+Using this information, your objectives are to:
+Predict the plant growth after a specific number of weeks.
+Implement control flow to make decisions on whether the plants should be:
+Pruned, to stop them from exceeding the capacity of the garden.
+This condition should be met if the plant count after the given number of weeks is greater than 80% of the maximum capacity of the garden.
+Monitored, if they are growing at an acceptable rate.
+This condition should be met if the plant count is between 50% and 80% of the maximum capacity of the garden after the given number of weeks.
+Planted, if there is room to plant more plants.
+This condition should be met if the plant count after the given number of weeks is less than 50% of the maximum capacity of the garden.
+Within your submission, include the results for 1, 2, and 3 weeks of growth as inputs
---
+++ script.js
@@ -0,0 +1,19 @@
+const PI=31415;
+const Radius=5;
+const Plant_Space=0.8;
+const Area = PI * radius * radius;
+const Initial_Plants=20
+const Number_Plants=(n)Initial_Plants*2
+let n=number of weeks
+
+console console.log(PlantGrowth);1*Initial_Plants*2
+
+
+
+
+
+
+
+
+
+T
